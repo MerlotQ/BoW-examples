bdf46f3aa0bb48f7e269b1c8aa965a7b2ef0e1cd
rongbohou <rongbohou@163.com>
2016-10-31 21:23:21 +0800

    add files

---
+++ build/src/CMakeFiles/bow_sift.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bow_sift.dir/BoFSIFT.cpp.o"
+  "../../bin/bow_sift.pdb"
+  "../../bin/bow_sift"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bow_sift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
